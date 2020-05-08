94801de0b4fdbb1ec8b1ffa562fcaf7de07d8d8c
Glenn Renfro <grenfro@pivotal.io>
2020-05-08 16:25:33 -0400

    Updated

---
+++ payroll/src/main/java/io/spring/payroll/Payroll.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.spring.payroll;
+
+import java.util.Date;
+
+public class Payroll {
+		private String employeeTypeId;
+		private String lastName;
+		private String firstName;
+		private String middleInitial;
+		private double amount;
+		private Date checkDate;
+		private String employeeId;
+
+	public String getEmployeeTypeId() {
+		return employeeTypeId;
+	}
+
+	public void setEmployeeTypeId(String employeeTypeId) {
+		this.employeeTypeId = employeeTypeId;
+	}
+
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMiddleInitial() {
+		return middleInitial;
+	}
+
+	public void setMiddleInitial(String middleInitial) {
+		this.middleInitial = middleInitial;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public Date getCheckDate() {
+		return checkDate;
+	}
+
+	public void setCheckDate(Date checkDate) {
+		this.checkDate = checkDate;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+}
---
+++ payroll/src/main/java/io/spring/payroll/batch/PayrollProcessor.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.spring.payroll.batch;
+
+import java.util.Date;
+
+import io.spring.payroll.Employee;
+import io.spring.payroll.Payroll;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.cloud.deployer.spi.scheduler.ScheduleInfo;
+
+/**
+ * Generates the pay for the {@link Payroll} .
+ * @author Glenn Renfro
+ */
+public class PayrollProcessor<T, C extends ScheduleInfo> implements ItemProcessor<Employee, Payroll>{
+
+	private static final Logger logger = LoggerFactory.getLogger(PayrollProcessor.class);
+
+	public PayrollProcessor() {
+
+	}
+
+	@Override
+	public Payroll process(Employee employee){
+		Payroll  payroll = new Payroll();
+		payroll.setEmployeeId(employee.getEmployeeId());
+		payroll.setEmployeeTypeId(employee.getEmployeeTypeId());
+		payroll.setAmount(1000000);
+		payroll.setCheckDate(new Date());
+		payroll.setLastName(employee.getLastName());
+		payroll.setFirstName(employee.getFirstName());
+		payroll.setMiddleInitial(employee.getMiddleInitial());
+
+		return payroll;
+	}
+}
