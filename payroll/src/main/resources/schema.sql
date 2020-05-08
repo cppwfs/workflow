e52ace1122bc69b893bbf8d990f6059b273ea3e4
Glenn Renfro <grenfro@pivotal.io>
2020-05-08 16:24:21 -0400

    moved to correct package

--- payroll/src/main/resources/schema.sql
+++ payroll/src/main/resources/schema.sql
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-drop table payroll;
+DROP TABLE IF EXISTS payroll;
 
 CREATE TABLE payroll
 (
